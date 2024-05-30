268ded0c03ebd5827012efe9d323030d55a3ad02
Facundo Prieto <facundo.prieto321@gmail.com>
2024-05-30 00:51:59 -0300

    fix: api url in production

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,9 +3,7 @@ import { http, HttpResponse } from "msw";
 export const handlers = [
   http.get("/api/wildfire/:date/:hour", async ({ params }) => {
     try {
-      const response = await fetch(
-        `http://localhost:3000/api/${params.date}/T${params.hour}.json`
-      );
+      const response = await fetch(`/api/${params.date}/T${params.hour}.json`);
       const data = await response.json();
       const items = data?.data?.getPublicWildfireByDate?.items;
       if (!items) {
