5714b124a29904f9981cc1e2745fbb2e549ff98a
Facundo Prieto <facundo.prieto321@gmail.com>
2024-05-30 00:37:00 -0300

    feat: use components on map

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,74 @@
-import React from 'react';
+import { useEffect, useState } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer } from "react-leaflet";
+import { useSelector } from "react-redux";
+import { RootState } from "./store/store";
+import { useGetWildfiresQuery } from "./store/api/apiSlice";
+import { Typography } from "@mui/material";
+import { getSatelliteConfidence } from "./utils/satelliteConf";
+import { WildFireMarker } from "./components/WildfireMarker";
+import { MapBox } from "./components/MapBox";
+import { FiltersPanel } from "./components/FIlterPanel";
+import { WildfireListPanel } from "./components/WildfireListPanel";
 
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css'
-
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-
-import './App.css';
-
+export interface Wildfire {
+  id: string;
+  x: number;
+  y: number;
+  conf: number;
+  sat: string;
+  date: string;
+}
 
 L.Icon.Default.mergeOptions({
-	iconRetinaUrl:
-		"https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
-	iconUrl:
-		"https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
-	shadowUrl:
-		"https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-shadow.png",
+  iconRetinaUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-shadow.png",
 });
 
 function App() {
+  const { date, hour, visibleConfidences } = useSelector(
+    (state: RootState) => state.filters
+  );
+  const { selected } = useSelector((state: RootState) => state.wildfire);
+  const { data: wildfires } = useGetWildfiresQuery({ date, hour });
+  const [map, setMap] = useState<L.Map | null>(null);
+
+  useEffect(() => {
+    if (selected) {
+      map?.setView([selected.y, selected.x], 10);
+    }
+  }, [selected, map]);
+
   return (
     <div className="App">
-      <MapContainer style={{height: "100vh", width: "100%"}} center={[-34.60, -58.38]} zoom={13}>
-        <TileLayer
-          attribution="Google Maps"
-          url="http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-        />
-        <Marker position={[-34.60, -58.38]}>
-          <Popup>
-            Alo!. <br /> I'm a marker.
-          </Popup>
-        </Marker>
+      <MapContainer
+        ref={setMap}
+        style={{ height: "100vh", width: "100%" }}
+        center={[-10, -58.38]}
+        zoom={4}
+        attributionControl={false}
+        zoomControl={false}
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {wildfires?.items.map(
+          (item: Wildfire) =>
+            visibleConfidences.includes(
+              getSatelliteConfidence(item.sat, item.conf)
+            ) && <WildFireMarker key={item.id} item={item} />
+        )}
       </MapContainer>
+      <MapBox position={{ top: 10, right: 10 }}>
+        <Typography>
+          Focos del {date} a las {hour}h
+        </Typography>
+      </MapBox>
+      <FiltersPanel />
+      <WildfireListPanel wildfires={wildfires?.items || []} />
     </div>
   );
 }
